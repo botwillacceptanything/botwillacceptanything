fa9e1bb0dc3dbd812f264078134c189edd83d1a5
Ashley Winters <awinters@trianglemediacorp.com>
2015-04-09 23:34:49 +0000

    Voting is code review; don't let the author vote

--- voting.js
+++ voting.js
@@ -162,6 +162,7 @@ module.exports = function(config, gh) {
           var body = comments[i].body;
 
           if(user === config.user) continue; // ignore self
+          if(user === pr.user.login) continue; // ignore the requester
           if(!stargazers[user]) continue; // ignore people who didn't star the repo
 
           if(body.indexOf(':-1:') !== -1) votes[user] = false;
