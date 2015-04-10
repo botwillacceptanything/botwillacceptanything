d84eed0b9402984c4e9746b7b3df7f738d4879c1
zztemp <none>
2015-04-10 16:38:58 -0600

    Fixes based on suggestions

--- minecraft/minecraft.sh
+++ minecraft/minecraft.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 cd minecraft
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.3.jar nogui
+if ! pgrep java > /dev/null
+then
+  java -Xmx768M -Xms412M -jar minecraft_server.1.8.3.jar nogui &
+fi
