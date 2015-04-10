fa9e9b5ce4bf52684eec49e7bf76c55d5e111c33
Tomas Korcak <korczis@gmail.com>
2015-04-10 22:47:33 +0200

    Added config template - config.template.js

---
+++ config.template.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    module.exports = {
+        user: "YOUR_GITHUB_USERNAME",
+        repo: "botwillacceptanything",
+        githubAuth: {
+            type: "oauth",
+            token: "YOUR_OAUTH_TOKEN"
+        }
+    };
+}());
