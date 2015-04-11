cc177329913ed3deaf3bb1b597b872ca3b4a2a27
Tomas Korcak <korczis@gmail.com>
2015-04-11 20:52:23 +0200

    Fix run.sh script

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d -p 3000:3000 -p 8080:8080 --name couchbase korczis/couchbase
+docker run -d -p 3000:3000 -p 8080:8080 --name couchbase korczis/botwillacceptanything
