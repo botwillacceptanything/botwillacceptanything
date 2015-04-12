abfba9f074131621eb1102c9ba15365a5c40b651
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-04-12 21:53:00 +0200

    Add public config file

---
+++ config.public.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    module.exports = {
+        users: {
+            // 'foo': {
+            //     // typical hash and salt combination
+            //     'sha1': null,
+            //     'salt': null,
+
+            //     // other encryption methods may be added later
+            // }
+        },
+
+        vault: {
+            users: {
+                // 'foo': {
+                //     'keys': { // Access to keys containing object
+                //         'bar': {}, // object contains settings
+                //         'foobar': {},
+                //     }
+                // }
+            }
+        }
+    };
+}());
