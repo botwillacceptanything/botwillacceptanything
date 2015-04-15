45ccaedfcac81e4427b51028bb631c4879db8b7c
Spenser Jones <hello@spenserj.com>
2015-04-15 14:22:05 -0600

    Add IRC tests for PR events

--- tests/unit/irc.js
+++ tests/unit/irc.js
@@ -14,6 +14,17 @@
   define(deps, function (assert, deferred, ircMocks, config, irc, events) {
     var greeting = ':localhost 001 testbot:Welcome to the Internet Relay Chat Network testbot\r\n';
     var motd = ':localhost 422 testbot:\r\n';
+    var mockEvent = {
+      pull_request: {
+        number: 1,
+        title: 'Test PR',
+        html_url: 'http://example.com',
+        merged_at: null,
+        user: {
+          login: 'TestAuthor',
+        },
+      },
+    };
 
     // Bootstrap a new test of the IRC client.
     function bootstrapTest() {
@@ -50,6 +61,60 @@
             done();
           }
           assert.ifError('Channel not joined');
+        });
+      });
+
+      it('When a bot.pull_request.vote_started event occurs, it should speak', function (done) {
+        var mock = bootstrapTest();
+        var client = irc();
+        client.on('registered', function () {
+          events.emit('bot.pull_request.vote_started', mockEvent.pull_request);
+          setTimeout(function () { client.disconnect(); }, 0);
+        });
+        mock.on('end', function () {
+          var msgs = mock.getIncomingMsgs();
+          if (msgs.filter(function (line) {
+            return (line.indexOf('PRIVMSG ' + config.irc.channel) !== -1);
+          }).length === 1) {
+            done();
+          }
+          assert.ifError('Message not sent');
+        });
+      });
+
+      it('When a github.pull_request.closed event occurs, it should speak', function (done) {
+        var mock = bootstrapTest();
+        var client = irc();
+        client.on('registered', function () {
+          events.emit('github.pull_request.closed', mockEvent);
+          setTimeout(function () { client.disconnect(); }, 0);
+        });
+        mock.on('end', function () {
+          var msgs = mock.getIncomingMsgs();
+          if (msgs.filter(function (line) {
+            return (line.indexOf('PRIVMSG ' + config.irc.channel) !== -1);
+          }).length === 1) {
+            done();
+          }
+          assert.ifError('Message not sent');
+        });
+      });
+
+      it('When a github.pull_request.merged event occurs, it should speak', function (done) {
+        var mock = bootstrapTest();
+        var client = irc();
+        client.on('registered', function () {
+          events.emit('github.pull_request.merged', mockEvent);
+          setTimeout(function () { client.disconnect(); }, 0);
+        });
+        mock.on('end', function () {
+          var msgs = mock.getIncomingMsgs();
+          if (msgs.filter(function (line) {
+            return (line.indexOf('PRIVMSG ' + config.irc.channel) !== -1);
+          }).length === 1) {
+            done();
+          }
+          assert.ifError('Message not sent');
         });
       });
     });
