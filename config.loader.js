c972073e8c82bf0ff8b69808bafd1db9c703d580
Spenser Jones <hello@spenserj.com>
2015-04-16 11:01:50 -0600

    Config Loader should create a boilerplate for configs/custom.js

--- config.loader.js
+++ config.loader.js
@@ -13,29 +13,44 @@
 
   var err;
 
-  var exists = fs.existsSync('./configs/custom.js');
-  if (exists === true) { return prepareConfig(); }
-
-  err = fs.renameSync('./config.js', './configs/custom.js');
-  // If an error occurred, load the original config.js.
-  if (err) {
-    console.error(err);
-    console.error(err.stack);
-    deps[0] = './config.js';
+  // If configs/custom.js exists, load it immediately. We don't need to
+  // restructure the configs.
+  if (fs.existsSync('./configs/custom.js') === true) {
+    if (fs.existsSync('./config.js') === false) { updateLoader(); }
     return prepareConfig();
   }
 
-  err = fse.copySync(__filename, './config.js');
-  if (err) {
-    console.error(err);
-    console.error(err.stack);
+  // If config.js doesn't exist right now, copy the template.
+  if (fs.existsSync('./config.js') === false) {
+    fse.copySync('./config.template.js', './configs/custom.js');
+  } else {
+    // Otherwise, move the current config into the correct location.
+    err = fs.renameSync('./config.js', './configs/custom.js');
+    // If an error occurred, load the original config.js.
+    if (err) {
+      console.error(err);
+      console.error(err.stack);
+      deps[0] = './config.js';
+      return prepareConfig();
+    }
   }
+
+  // Update the loader and then prepare the config for merging.
+  updateLoader();
   prepareConfig();
+
+  function updateLoader() {
+    // Copy this loader into config.js for backwards compatibility.
+    err = fse.copySync(__filename, './config.js');
+    if (err) {
+      console.error(err);
+      console.error(err.stack);
+    }
+  }
 
   function prepareConfig() {
     var exists = fs.existsSync('./configs/' + process.env.BUILD_ENVIRONMENT + '.js');
 
-    console.log(process.env.BUILD_ENVIRONMENT);
     if (exists === true) {
       deps.push('./configs/' + process.env.BUILD_ENVIRONMENT + '.js');
     }
