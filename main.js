c5904debbc67058e9045a204f48bd28fc4ba73fc
David Brooks Pokorny <dbpokorny@gmail.com>
2015-04-16 13:16:20 -0700

    Remove devmain

--- main.js
+++ main.js
@@ -96,25 +96,6 @@
         });
     }
 
-    // Use for testing so bot doesn't overwrite my work
-    function devmain() {
-        // find the hash of the current HEAD
-        head(function (err, current) {
-            if (err) { return console.error('error checking HEAD:', err); }
-
-            console.log('Bot is initialized. HEAD:', current);
-            considerExistence();
-
-            // greet the other bots
-            talk.speak();
-
-            // Allow the voting system to bootstrap and begin monitoring PRs.
-            voting.initialize();
-        });
-    }
-
-    // Last thing before commit: change devmain back to main
-
     mocks()
       .then(integrations)
       .then(main, function (err) {
