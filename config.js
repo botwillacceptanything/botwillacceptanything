ac7280b3de1d8d80af46277bfcb44f7ebfbb25e4
Spenser Jones <hello@spenserj.com>
2015-04-21 15:28:28 -0600

    Clean up config loader, since we no longer run two files for config support

--- config.js
+++ config.js
@@ -13,40 +13,13 @@
 
   var err;
 
-  // If configs/custom.js exists, load it immediately. We don't need to
-  // restructure the configs.
-  if (fs.existsSync('./configs/custom.js') === true) {
-    if (fs.existsSync('./config.js') === false) { updateLoader(); }
-    return prepareConfig();
-  }
-
-  // If config.js doesn't exist right now, copy the template.
-  if (fs.existsSync('./config.js') === false) {
+  // If configs/custom.js doesn't exist right now, copy the template.
+  if (fs.existsSync('./configs/custom.js') === false) {
     fse.copySync('./configs/template.js', './configs/custom.js');
-  } else {
-    // Otherwise, move the current config into the correct location.
-    err = fs.renameSync('./config.js', './configs/custom.js');
-    // If an error occurred, load the original config.js.
-    if (err) {
-      console.error(err);
-      console.error(err.stack);
-      deps[0] = './config.js';
-      return prepareConfig();
-    }
   }
 
   // Update the loader and then prepare the config for merging.
-  updateLoader();
   prepareConfig();
-
-  function updateLoader() {
-    // Copy this loader into config.js for backwards compatibility.
-    err = fse.copySync(__filename, './config.js');
-    if (err) {
-      console.error(err);
-      console.error(err.stack);
-    }
-  }
 
   function prepareConfig() {
     var exists = fs.existsSync('./configs/' + process.env.BUILD_ENVIRONMENT + '.js');
