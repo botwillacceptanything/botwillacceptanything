b6597cd9e42cea3324fb25c689fce9237c5600b2
Yong Cho Yin <hkminegod@users.noreply.github.com>
2015-09-19 15:45:31 +0800

    Update template.js

--- configs/template.js
+++ configs/template.js
@@ -38,7 +38,7 @@
          period: 15,
          period_jitter: 0.2,
          minVotes: 7,
-         supermajority: 0.65,
+         supermajority: 0.5,
          pollInterval: 3, // Minutes
        },
 
